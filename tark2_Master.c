c1db3a6c2535702ed8f9b2cc8f4af8fff289b84f
Do Van Chien <77058748+dvchi159@users.noreply.github.com>
2021-02-05 00:21:44 +0700

    Update tark2_Master.c
    File output

--- tark2_Master.c
+++ tark2_Master.c
@@ -1,3 +1,4 @@
+ 
 #include <stdio.h>
 int intInput(const char* mess){
   int inputValue;
@@ -5,26 +6,39 @@ int intInput(const char* mess){
   scanf("%d", &inputValue);
   return inputValue;
 }
-void tableHeader(){
-	printf("\nDEPRECIATION TABLE\n------------------\n");
-	printf("\t\t\t\t\t\tACCUMULATED\n");
-	printf("YEAR\tDEPRECIATION\tEND-OF-YEAR VALUE\tDEPRECIATION\n");
-	printf("----\t------------\t-----------------\t--------------------\n");
-}
 void tableDepreciation() {
+  FILE *outputFile = fopen("/home/fdc/Documents/Code_OUT/Test.txt", "w");
   int machineCost, depreciationRate, year;
-  machineCost = intInput("Input Machine Cost: ");
-  depreciationRate = intInput("Input Depreciation Rate: ");
-  year = intInput("Input Number of Year: ");
-  tableHeader();
+  {//Input Value
+    machineCost = intInput("Input Machine Cost: ");
+    fprintf(outputFile, "Machine cost: %d\n", machineCost);
+    depreciationRate = intInput("Input Depreciation Rate: ");
+    fprintf(outputFile, "Depreciation rate: %d\n", depreciationRate);
+    year = intInput("Input Number of Year: ");
+    fprintf(outputFile, "Year: %d\n", year);
+  }
+  {//Table Header
+    printf("\nDEPRECIATION TABLE\n------------------\n");
+    fprintf(outputFile,"\nDEPRECIATION TABLE\n------------------\n");
+    printf("\t\t\t\t\t\tACCUMULATED\n");
+    fprintf(outputFile,"\t\t\t\t\t\tACCUMULATED\n");
+    printf("YEAR\tDEPRECIATION\tEND-OF-YEAR VALUE\tDEPRECIATION\n");
+    fprintf(outputFile,"YEAR\tDEPRECIATION\tEND-OF-YEAR VALUE\tDEPRECIATION\n");
+    printf("----\t------------\t-----------------\t--------------------\n");
+    fprintf(outputFile,"----\t------------\t-----------------\t--------------------\n");
+  }
   for (int i = 1; i <= year; ++i){
     printf("%d\t%d\t\t%d\t\t\t%d\n",i, depreciationRate, (machineCost-depreciationRate*i), depreciationRate*i);
+    fprintf(outputFile,"%d\t%d\t\t%d\t\t\t%d\n",i, depreciationRate, (machineCost-depreciationRate*i), depreciationRate*i);
     if ((machineCost-depreciationRate*i) <= 0){
       printf("Machine is out of date.\n");
+      fprintf(outputFile,"Machine is out of date.\n");
       break;
     }
   }
+  fclose(outputFile);
 }
-void main(){
+int main(){
   tableDepreciation();
+  printf("Report file in '/home/fdc/Documents/Code_OUT/Test.txt' ");
 }
